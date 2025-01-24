7a7a4593455d947d8ffe9a13bf1dcd0b70afd375
7Caylon7 <caylon.costa@itec.ufpa.br>
2025-01-24 15:09:01 -0300

    Refatoração dos scripts JS

--- script.js
+++ script.js
@@ -8,132 +8,85 @@ const elements = {
 };
 
 const musics = {
-    background: {
-        element: document.getElementById("backgroundMusic"),
-    },
-    crash: {
-        element: document.getElementById("crashMusic"),
-    },
-    running: {
-        element: document.getElementById("runningMusic"),
-    },
-    jump: {
-        element: document.getElementById("jumpMusic"),
-    },
-    gameover: {
-        element: document.getElementById("gameoverMusic"),
-    },
+    background: document.getElementById("backgroundMusic"),
+    crash: document.getElementById("crashMusic"),
+    running: document.getElementById("runningMusic"),
+    jump: document.getElementById("jumpMusic"),
+    gameover: document.getElementById("gameoverMusic"),
 };
 
-
-function playBackgroundMusic(){
-    musics.background.element.play();
-}
-
-function pauseBackgroundMusic(){
-    musics.background.element.pause();
-}
-
-function playRunningMusic(){
-    musics.running.element.play();
-    musics.running.element.volume = 0.1;
-}
-
-function pauseRunningMusic(){
-    musics.running.element.pause();
-}
-
-function playJumpMusic(){
-    musics.jump.element.play();
-    musics.jump.element.volume = 0.1;
-}
-
-function playCrashMusic(){
-    musics.crash.element.play();
-}
-
-function playGameoverMusic(){
-    musics.gameover.element.play();
-}
-
-function pauseGameoverMusic(){
-    musics.gameover.element.pause();
-}
+const musicFunctions = {
+    play: (musicName, volume = 1) => {
+        const music = musics[musicName];
+        if (music) {
+            music.play();
+            music.volume = volume;
+        }
+    },
+    pause: (musicName) => {
+        const music = musics[musicName];
+        if (music) music.pause();
+    }
+};
 
 function updateCounter(count) {
     elements.counterElement.textContent = count;
 }
 
-// Funções para salto
 function jump() {
     elements.persona.classList.add("jump");
-  
+
     setTimeout(() => {
-      elements.persona.classList.remove("jump");
+        elements.persona.classList.remove("jump");
     }, 500);
-  }
+}
 
-function startGame() {
+function stopGame(lapidePosition, birdPosition, personaPosition) {
+    musicFunctions.pause('background');
+    musicFunctions.pause('running');
+    musicFunctions.play('gameover');
+    
+    elements.lapide.style.animation = "none";
+    elements.lapide.style.left = `${lapidePosition}px`;
+    elements.bird.style.animation = "none";
+    elements.bird.style.left = `${birdPosition}px`;
+    elements.persona.style.animation = "none";
+    elements.persona.style.left = `${personaPosition}px`;
+    elements.persona.src = "./img/personagem-death.gif";
+    elements.persona.style.width = "60px";
+    elements.gameover.classList.remove("hidden");
+    elements.restartButton.classList.remove("restart");
+}
 
+function startGame(){
     let count = 0;
 
-    updateCounter(count);
+    function incrementCounter(){
+        count++;
+        updateCounter(count);
+    }
+
+    document.addEventListener("keydown", jump);
 
     const loop = setInterval(() => {
         const lapidePosition = elements.lapide.offsetLeft;
         const birdPosition = elements.bird.offsetLeft;
-        const personaPosition = +window.getComputedStyle(elements.persona).bottom.replace("px", "");
-        
-        playBackgroundMusic();
-        playRunningMusic();
+        const personaPosition = +window.getComputedStyle(elements.persona).bottom.replace("px","");
 
-        if (lapidePosition <= 60 && lapidePosition > 0 && personaPosition < 91){
+        musicFunctions.play('background');
+        musicFunctions.play('running');
 
-            pauseBackgroundMusic();
-            pauseRunningMusic();
-            // playCrashMusic();
-            playGameoverMusic();
-
-            elements.lapide.style.animation = "none";
-            elements.lapide.style.left = `${lapidePosition}px`;
-
-            elements.bird.style.animation = "none";
-            elements.bird.style.left = `${birdPosition}px`;
-
-            elements.persona.style.animation = "none";
-            elements.persona.style.left = `${personaPosition}px`;
-
-            elements.persona.src = "./img/personagem-death.gif";
-            elements.persona.style.width = "60px";
-            elements.gameover.classList.remove("hidden");
-            elements.restartButton.classList.remove("restart");
-
-        }else{
+        if(lapidePosition <= 60 && lapidePosition > 0 && personaPosition < 91) {
+            stopGame(lapidePosition, birdPosition, personaPosition);
+            clearInterval(loop);
+        } else {
             incrementCounter();
         }
 
-        if(personaPosition > 90){
-            playJumpMusic();
+        if (personaPosition > 90) {
+            musicFunctions.play('jump');
         }
-
-    }, 10);
-
-    document.addEventListener("keydown", jump);
-
-    function incrementCounter() {
-        count++;
-        updateCounter(count);
-    }
+    }, 1)
 }
 
 startGame();
-
-// elements.restartButton.addEventListener("click", () => {
-//     elements.gameover.classList.add("hidden");
-//     elements.restartButton.classList.add("restart");
-//     pauseGameoverMusic();
-
-//     elements.lapide.style.animation = "lapide-animation 1.5s linear infinite";
-
-//     elements.persona.src = "./img/personagem.gif";
-// });
